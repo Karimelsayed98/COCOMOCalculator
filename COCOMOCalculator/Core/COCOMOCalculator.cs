c41f907a4f8fc5e0a91fa0513dc3870858da5dfc
chantal <chanty98@hotmail.com>
2019-12-11 13:26:01 +0200

    Implemented calculator core functions

--- COCOMOCalculator/Core/COCOMOCalculator.cs
+++ COCOMOCalculator/Core/COCOMOCalculator.cs
@@ -1,9 +1,12 @@
-﻿namespace COCOMOCalculator.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace COCOMOCalculator.Core
 {
     public class COCOMOCalculator
     {
         private COCOMOMode _mode;
-        private float[][] COCOMOCoefficients;
+        private double[][] COCOMOCoefficients;
 
         public COCOMOCalculator(COCOMOMode mode)
         {
@@ -11,13 +14,45 @@
             this.initializeCoefficients();
         }
 
-        public float CalculateEffort(int KLOC)
+        public double CalculateEffort(int KLOC)
         {
-            throw new System.Exception("Not Implemented");
+            return this.getCoefficient('a') * (Math.Pow(KLOC, this.getCoefficient('b')));
+        }
+
+        public double CalculateTDev(double effort)
+        {
+            return this.getCoefficient('c') * (Math.Pow(effort, this.getCoefficient('d')));
+        }
+
+        public double CalculateEAF(List<double> costDriversValues)
+        {
+            double result = 1.0;
+            foreach (var value in costDriversValues)
+            {
+                result *= value;
+            }
+
+            return result;
+        }
+
+        public double CalculateActualEffort(double effort, double EAF)
+        {
+            return effort * EAF;
+        }
+
+        private double getCoefficient(char index)
+        {
+            return this.COCOMOCoefficients[(int) this._mode][index - 'a'];
         }
 
         private void initializeCoefficients()
         {
+            this.COCOMOCoefficients = new double[][]
+            {
+                new double[] { 2.4F, 1.05F, 2.5F, 0.38F },
+                new double[] { 3.0F, 1.12F, 2.5F, 0.35F },
+                new double[] { 3.6F, 1.20F, 2.5F, 0.32F },
+            };
         }
     }
 }
---
+++ COCOMOCalculator/Core/CostDriver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace COCOMOCalculator.Core
+{
+    public class CostDriver
+    {
+        public string Name;
+        public Dictionary<CostDriverRating, double> RatingValue;
+
+        public CostDriver(string Name, Dictionary<CostDriverRating, double> RatingValue)
+        {
+            this.Name = Name;
+            this.RatingValue = RatingValue;
+        }
+    }
+}
